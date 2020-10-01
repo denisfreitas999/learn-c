cbf5ea50b1e49228b99dc7a9d392a7f8a6e24839
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-01 19:27:59 -0300

    50Aula-Operadores Logicos - finalizada

---
+++ 50Aula-Operadores Logicos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void){
+
+    int idade;
+
+    printf("Digite sua idade:\n");
+    scanf("%i", &idade);
+
+    if(idade >= 20 && idade <= 40){
+        printf("deu certo\n");
+    }else
+    {
+        printf("nao deu certo\n");
+    }
+    
+
+    return 0;
+}
