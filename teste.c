57c4da8ecdbb505b2b0d8b4e5d76045ea1d23078
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-10-01 21:38:59 -0300

    51Aula-Verificar Igualdade Entre Duas Strings - Finalizada

--- teste.c
+++ teste.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
 
-int main (void){
+int main(void){
 
- 
-    char novaPalavra[13];
+    _Bool stringsIguais(char s1[], char s2[]);
 
-    for (int i = 0; i < 13; i++)
-    {
-        printf("%c", novaPalavra[i]);
-    }
+
+    stringsIguais("casa", "cara");
+
     
-    printf("\n");
-
-
 
     return 0;
 }
+
+_Bool stringsIguais(char s1[], char s2[]){
+
+   int num1Caracteres = 0;
+   int num2Caracteres = 0;
+
+   while (s1[num1Caracteres] != '\0')
+   {
+       ++num1Caracteres;
+   }
+    
+   while (s2[num2Caracteres] != '\0')
+   {
+       ++num2Caracteres;
+   }
+    
+    if (num1Caracteres != num2Caracteres)
+    {
+        printf("As strings sao diferentes.\n");
+    }else
+    {
+        
+            for (int i = 0; i < num1Caracteres; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    printf("As strings sao diferentes.\n");
+                }
+                
+                
+            }
+            
+    }
+    
+    
+
+}
+
+
