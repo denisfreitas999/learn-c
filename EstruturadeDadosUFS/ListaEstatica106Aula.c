02f7aae5dc40154a0634c8f736f7c989413d2620
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-11-22 00:19:26 -0300

    Adicionados Listas

---
+++ EstruturadeDadosUFS/ListaEstatica106Aula.c
@@ -0,0 +1,19 @@
+//Arquivo ListaEstatica106Aula.c
+#include <stdio.h>
+#include <stdio.h>
+#include "ListaEstatica106Aula.h"
+
+struct lista
+{
+    int qtd;
+    struct aluno dados [MAX];
+};
+
+
+
+int main(void){
+    Lista *li;
+    
+    return 0;
+}
+
---
+++ EstruturadeDadosUFS/ListaEstatica106Aula.h
@@ -0,0 +1,11 @@
+//Arquivo ListaEstatica106Aula.h
+#define MAX int 100
+
+struct aluno{
+    int matricula;
+    char nome[30];
+    float nota1,nota2,nota3;
+};
+typedef struct lista Lista;
+
+
